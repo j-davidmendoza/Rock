89f18f76c40455df7a55f83a5c5e1b73022cb823
David Mendoza <mendoza.d.jesus@gmail.com>
2025-04-27 21:08:06 -0700

    - Task Two B: Added GroupAllGroupsPicker Obsidian control and integrated API support, only API was tested.

---
+++ Rock.JavaScript.Obsidian/Framework/ViewModels/Rest/Controls/groupPickerGetAllGroupsOptionsBag.d.ts
@@ -0,0 +1,8 @@
+/**
+ * The options that can be passed to the GroupPickerGetAllGroups API action of
+ * the groupAllGroupsPicker control.
+ */
+export type GroupPickerGetAllGroupsOptionsBag = {
+    /** Whether to include inactive groups or not. */
+    includeInactiveGroups: boolean;
+};
